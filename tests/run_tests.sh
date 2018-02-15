e7d7c866a6dff7d501de3be33e8b3e1da7b981cc
Aram Bayadyan <aram.bv@gmail.com>
2018-02-15 06:39:44 +0400

    Disable PhantomJS test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -17,5 +17,5 @@ bash ./test_meteor_app_with_devbuild.sh
 # bash ./test_binary_build_on_base.sh
 # bash ./test_binary_build_on_bin_build.sh
 
-bash ./test_phantomjs.sh
+# bash ./test_phantomjs.sh
 bash ./test_no_app.sh
